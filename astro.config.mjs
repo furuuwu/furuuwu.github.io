3e34f44c9286d8f1c30d279b8670fe8735aa3b4c
Your Name <you@example.com>
2023-08-30 23:54:31 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 
-const me = "ricfa.lbg";
+const me = "furuuwu";
 
 // https://astro.build/config
 export default defineConfig({
